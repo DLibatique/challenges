034d0c39b0b1ced7e9452ed35cd8eb48bd4ed438
Daniel Libatique <daniel.libatique@gmail.com>
2020-07-01 21:58:55 -0400

    started day 3

--- 03/tags-help.py
+++ 03/tags-help.py
@@ -5,7 +5,7 @@ import re
 
 IDENTICAL = 1.0
 TOP_NUMBER = 10
-RSS_FEED = 'rss.xml'
+RSS_FEED = '/Users/daniellibatique/code/github-repos/challenges/03/rss.xml'
 SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
 
@@ -14,14 +14,21 @@ def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
     Replace dash with whitespace.
     Hint: use TAG_HTML.findall"""
-    pass
+    with open(RSS_FEED) as f:
+        raw = f.read()
+    return re.findall(TAG_HTML, raw)
 
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
     Hint: use most_common method of Counter (already imported)"""
-    pass
+    return Counter(tags).most_common()
 
+test = get_tags()
+print(product(get_top_tags(test)[0], get_top_tags(test)[1]))
+
+for x in get_top_tags(test):
+    product(x[0], )
 
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR
@@ -30,15 +37,15 @@ def get_similarities(tags):
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
     pass
 
-
-if __name__ == "__main__":
-    tags = get_tags()
-    top_tags = get_top_tags(tags)
-    print('* Top {} tags:'.format(TOP_NUMBER))
-    for tag, count in top_tags:
-        print('{:<20} {}'.format(tag, count))
-    similar_tags = dict(get_similarities(tags))
-    print()
-    print('* Similar tags:')
-    for singular, plural in similar_tags.items():
-        print('{:<20} {}'.format(singular, plural))
+#
+# if __name__ == "__main__":
+#     tags = get_tags()
+#     top_tags = get_top_tags(tags)
+#     print('* Top {} tags:'.format(TOP_NUMBER))
+#     for tag, count in top_tags:
+#         print('{:<20} {}'.format(tag, count))
+#     similar_tags = dict(get_similarities(tags))
+#     print()
+#     print('* Similar tags:')
+#     for singular, plural in similar_tags.items():
+#         print('{:<20} {}'.format(singular, plural))
